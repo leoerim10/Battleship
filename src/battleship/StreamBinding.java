faee95bfad749e232b3863261abbb2a1a3a140e6
leoerim10 <63184731+leoerim10@users.noreply.github.com>
2020-06-09 15:06:25 +0200

    Delete StreamBinding.java

--- src/battleship/StreamBinding.java
+++
@@ -1,8 +0,0 @@
-package battleship.protocolBinding;
-
-public class StreamBinding {
-    public static final int DICE = 0;
-    public static final int SET = 1;
-    public static final int GIVE_UP = 2;
-    public static final int SHIP_STATUS = 3;
-}
